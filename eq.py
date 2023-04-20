39c20b97e216f3c14075a80c2694cce2efb7859e
WillRobinson152 <robinsonwilliamarthur@gmail.com>
2023-04-20 15:35:42 -0500

    Moved url construction inside of getDf method and deleted query method.

--- eq.py
+++ eq.py
@@ -5,9 +5,9 @@ Petroleum Status Report.
 
 Typical usage example:
 
-  get = EiaQuery('your_api_key')
-  url = get.query('petroleum/',
-                  'stoc/wstk/',
+  query = EiaQuery('your_api_key')
+  df = query.getDf('petroleum',
+                  'move/wkly',
                   'weekly',
                   {'series': ['W_EPLLPZ_EEX_NUS-Z00_MBBLD', 
                               'WPRIM_NUS-Z00_2']
@@ -15,7 +15,6 @@ Typical usage example:
                   '2023-04-12',
                   '2023-04-19',
                  )
-  df  = get.getDf(url) 
 """
 
 import requests
@@ -34,10 +33,9 @@ class EiaQuery:
         key: API key provided at initialization of class.
     """
     def __init__(self, apiKey):
-        self.urlStart = 'https://api.eia.gov/v2/'
-        self.key = f'?api_key={apiKey}&'
+        self.key = apiKey
 
-    def addParameters(self, freq, facets, start=None, end=None):
+    def _addParameters(self, freq, facets, start=None, end=None):
         s = f'frequency={freq}&data[0]=value'
         for k, v in facets.items():
             for item in v:
@@ -48,23 +46,16 @@ class EiaQuery:
             s += f'&end={end}'
         return s
     
-    def query(self,
-              sub1,
-              sub2,
-              freq,
+    def getDf(self, 
+              sub1, 
+              sub2, 
+              freq, 
               facets,
               start=str(date.today() - relativedelta(years=3)),
-              end=None):
-        params = self.addParameters(
-            freq=freq,
-            facets=facets,
-            start=start,
-            end=end
-        )
-        url = f'{self.urlStart}{sub1}{sub2}data/{self.key}{params}'
-        return url
-    
-    def getDf(self, url):
+              end=None
+              ):
+        url = f'https://api.eia.gov/v2/{sub1}/{sub2}/data/?api_key={self.key}&'
+        url += self._addParameters(freq, facets, start, end)
         r = requests.get(url)
         df = pd.DataFrame(data=r.json()['response']['data'])
         df['period'] = pd.to_datetime(df.period).dt.date
