a7bac08e377074d4452882398ed9a24e6fb3957c
WillRobinson152 <robinsonwilliamarthur@gmail.com>
2023-04-21 12:21:49 -0500

    Added method docstring for getDf.

--- eq.py
+++ eq.py
@@ -54,6 +54,24 @@ class EiaQuery:
               start=str(date.today() - relativedelta(years=3)),
               end=None
               ):
+        """Fetches data from EIA API and returns Pandas DataFrame.
+
+        Creates url based on sub1, sub2, frequency, facets and start and end 
+        date parameters. Sends get request to url with class's API key. Returns
+        response data as Pandas DataFrame.
+
+        Args:
+            sub1: String of first element in url after API key.
+            sub2: String of second element in url after API key. For some requests,
+            this element will have mutliple words separated by '/'.
+            freq: String allowed by EIA (example: 'Weekly').
+            facets: Dictionary with facet categories allowed by EIA ('Product' or 
+            'Series', for example) as keys and lists of EIA codes for each category
+            as values.
+
+        Returns:
+            A Pandas DataFrame with date values set to datetime.date.
+        """
         url = f'https://api.eia.gov/v2/{sub1}/{sub2}/data/?api_key={self.key}&'
         url += self._addParameters(freq, facets, start, end)
         r = requests.get(url)
